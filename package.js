6e6afa4f60a86db0e52b2f254daf46f63af8b639
trihatmaja <trihatmaja@gmail.com>
2016-03-24 21:44:08 +0700

    date time picker

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'netanelgilad:ionic-datepicker',
-  version: '0.0.3',
+  name: 'trihatmaja:ionic-datetimepicker',
+  version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'ionic datepicker',
+  summary: 'ionic datetimepicker',
   // URL to the Git repository containing the source code for this package.
-  git: 'http://github.com/netanelgilad/meteor-ionic-datepicker',
+  git: 'http://github.com/trihatmaja/meteor-ionic-datepicker',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,18 +13,17 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('urigo:ionic@1.0.0', 'client');
-  api.use('angular:angular@1.3.15_1', 'client');
+  api.use('driftyco:ionic@1.0.0', 'client');
+  api.use('angular:angular@1.3.7', 'client');
 
   api.addFiles([
-    'templates.js',
-    'ionic-datepicker.js',
+    'ionic-datetimepicker.js',
     'style.css'
   ], 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('netanelgilad:ionic-datepicker');
+  api.use('trihatmaja:ionic-datepicker');
   api.addFiles('ionic-datepicker-tests.js');
 });
